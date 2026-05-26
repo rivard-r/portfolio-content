a58084047c838aa207495761907d0d293e097b60
Ryan Rivard <ryan.j.rivard@protonmail.com>
2026-05-25 19:05:45 -0700

    Modified to ignore md files with prefix i_ functioning as a do not publish flag

--- generate-diagrams.sh
+++ generate-diagrams.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# rm diagrams/*
-# rm processed-markdown/*
-#
-# for file in *.md 
-# do
-#   PROCESSED_FILE_NAME=${file%.*}
-#   mmdc -i "${file}" -o "./processed-markdown/${PROCESSED_FILE_NAME}_p.md" \
-#     -a "./diagrams"
-# done
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECTS_DIR="${1:-"${SCRIPT_DIR}/projects"}"
 DIAGRAMS_DIR=${PROJECTS_DIR}"/diagrams"
@@ -25,6 +16,8 @@ do
   FILE_PATH_EXTENSION_TRIMMED=${file%.md}
   FILE_NAME=${FILE_PATH_EXTENSION_TRIMMED##${PROJECTS_DIR}/}
   echo "File name: ${FILE_NAME}" 
-  mmdc -i "${file}" -o "${PROCESSED_MARKDOWN_DIR}/${FILE_NAME##*\/}_p.md" \
+  if [[ ${FILE_NAME} != i_* ]]; then
+    mmdc -i "${file}" -o "${PROCESSED_MARKDOWN_DIR}/${FILE_NAME##*\/}_p.md" \
     -a "${DIAGRAMS_DIR}"
+  fi
 done
